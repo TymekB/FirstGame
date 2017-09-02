7dd1be2fa108222fdd113ec775808e0e570c27e0
TymekB <baran.tymoteusz@gmail.com>
2017-09-02 21:00:53 +0200

    added background and platforms

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, cre
 
 function preload()
 {
-    game.load.spritesheet('player', 'assets/dude.png');
+    game.load.spritesheet('player', 'assets/dude.png', 32, 48);
     game.load.image('platform', 'assets/platform.png');
     game.load.image('sky', 'assets/sky.png');
     game.load.image('star', 'assets/star.png');
@@ -10,7 +10,17 @@ function preload()
 
 function create()
 {
-    
+    game.add.sprite(0,0, 'sky');
+
+    var platforms = game.add.group();
+
+    var ground1, ground2;
+    ground1 = platforms.create(0, game.world.height-32, 'platform');
+    ground2 = platforms.create(400, game.world.height-32, 'platform');
+
+    var platform1, platform2;
+    platform1 = platforms.create(500, 360, 'platform');
+    platform2 = platforms.create(-150, 200, 'platform');
 }
 
 function update()
