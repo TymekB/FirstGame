8418932d8a20691b4f3c1738ffb7663958915ccd
TymekB <baran.tymoteusz@gmail.com>
2017-09-02 21:26:11 +0200

    added player

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
+var player;
+var platforms;
 
 function preload()
 {
@@ -12,20 +14,32 @@ function create()
 {
     game.add.sprite(0,0, 'sky');
 
-    var platforms = game.add.group();
+    platforms = game.add.group();
+    platforms.enableBody = true;
 
     var ground1, ground2;
     ground1 = platforms.create(0, game.world.height-32, 'platform');
     ground2 = platforms.create(400, game.world.height-32, 'platform');
 
+    ground1.body.immovable = true;
+    ground2.body.immovable = true;
+
     var platform1, platform2;
     platform1 = platforms.create(500, 360, 'platform');
     platform2 = platforms.create(-150, 200, 'platform');
+
+    platform1.body.immovable = true;
+    platform2.body.immovable = true;
+
+    player = game.add.sprite(50, game.world.height-150, 'player');
+
+    game.physics.enable(player);
+    player.body.gravity.y = 300;
 }
 
 function update()
 {
-
+    game.physics.arcade.collide(player, platforms);
 }
 
 
